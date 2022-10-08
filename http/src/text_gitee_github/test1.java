3dce9891b03e84b6c7302a9ac8b0d63b02f48979
JIE <3492779706@qq.com>
2022-10-08 23:04:12 +0800

    test gitee and github

---
+++ http/src/text_gitee_github/test1.java
@@ -0,0 +1,15 @@
+package text_gitee_github;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: JIE
+ * Date: 2022-10-08
+ * Time: 23:00
+ */
+public class test1 {
+    public static void main(String[] args) {
+
+        System.out.println("test gitee and github");
+    }
+}
