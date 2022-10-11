a1215e9e78dcda63c1b31fe3171d39407f437465
JIE <3492779706@qq.com>
2022-10-11 20:03:45 +0800

    第一次写Maven代码

---
+++ first_servlet/src/main/java/hello_serlvet.java
@@ -0,0 +1,12 @@
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: JIE
+ * Date: 2022-10-11
+ * Time: 20:01
+ */
+public class hello_serlvet extends HttpServlet {
+
+}
