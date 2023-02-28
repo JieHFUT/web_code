af63f02ba4bbde439450cdd56e001b60498850cf
Jie <9594523wrj@gmail.com>
2023-02-28 19:23:56 +0800

    log commit

--- spring_boot/src/main/java/com/jiehfut/spring_boot/TestController.java
+++ spring_boot/src/main/java/com/jiehfut/spring_boot/TestController.java
@@ -1,16 +1,26 @@
 package com.jiehfut.spring_boot;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+
+
+    // 获取用户自己配置的信息
+
+    @Value("${mykey.key}")
+    private String mykey;
+
+    @Value("${server.port}")
+    private Integer port;
+
+
     @RequestMapping("/sayHi")
     public String sayHi() {
-        return "Hi. ";
+        return "Hi. " + mykey + "| port:  " + port;
     }
-
-
 }
