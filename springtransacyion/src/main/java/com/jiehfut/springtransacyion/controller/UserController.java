e4c4c01f2c109a572c44d90c60f69c63ab11c871
Jie <9594523wrj@gmail.com>
2023-03-12 19:34:28 +0800

    spring transactional

--- springtransacyion/src/main/java/com/jiehfut/springtransacyion/controller/UserController.java
+++ springtransacyion/src/main/java/com/jiehfut/springtransacyion/controller/UserController.java
@@ -3,7 +3,9 @@ package com.jiehfut.springtransacyion.controller;
 
 import com.jiehfut.springtransacyion.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,14 +18,22 @@ public class UserController {
     UserService userService;
 
     @RequestMapping("/add")
-    @Transactional
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    // transacyional在下面方法中出现异常的时候就会发生回滚
     public int add(String username, String password) {
         if (!StringUtils.hasLength(username) && !StringUtils.hasLength(password)){
             return 0;
         }
         int result = userService.add(username, password);
         System.out.println("执行了UserController的add方法！");
-        // int testTransactional = 10 / 0;
+        try{
+            int testTransactional = 10 / 0;
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            // throw e;
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+        }
         return result;
+
     }
 }
