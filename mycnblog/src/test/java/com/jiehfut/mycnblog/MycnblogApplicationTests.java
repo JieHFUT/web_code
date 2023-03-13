0f0f6a0ef083256f15d418b94dd6a5829c6e4179
Jie <9594523wrj@gmail.com>
2023-03-13 12:57:23 +0800

    myblog

---
+++ mycnblog/src/test/java/com/jiehfut/mycnblog/MycnblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiehfut.mycnblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycnblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
